6185af9efa36081e6bf5c4f283a89879f04fb758
MacAnThony Emmanuel <macanthonyemmanuel12@gmail.com>
2024-05-21 06:53:06 +0100

    implemented mail function

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -1,8 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const WEB_PORT = "80"
+
 type Config struct {
 }
 
 func main() {
-
+	app := Config{}
+	log.Println("starting mail service on port", WEB_PORT, "🚀🚀🚀")
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%s", WEB_PORT),
+		Handler: app.routes(),
+	}
+	err := srv.ListenAndServe()
+	if err != nil{
+		log.Panic(err)
+	}
 }
